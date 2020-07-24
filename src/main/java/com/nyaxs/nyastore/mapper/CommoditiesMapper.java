4b08f65c735256e527d05fc9fd93a70e22da61eb
nyaxs <nyaxs@outlook.com>
2020-07-24 17:56:35 +0800

    --

---
+++ src/main/java/com/nyaxs/nyastore/mapper/CommoditiesMapper.java
@@ -0,0 +1,35 @@
+package com.nyaxs.nyastore.mapper;
+
+import com.nyaxs.nyastore.entity.Commodities;
+import com.nyaxs.nyastore.entity.Members;
+import org.apache.ibatis.annotations.*;
+
+/**
+ * @ClassName CommoditiesMapper
+ * @Description TODO:
+ * @Author nyaxs
+ * @Date 2020-07-24 17:20
+ * @Version 1.0
+ **/
+
+@Mapper
+public interface CommoditiesMapper {
+
+    @Select("select * from commodities where name = #{name} ")
+    Commodities getCommodityByName(String name);
+
+    @Select("select * from commodities where id = #{id}")
+    Commodities selectById(int id);
+
+    @Insert("insert into commodities(name,price) values(#{name},#{password})")
+    int insertMemberByRegister(Commodities commodity);
+
+    @Select("select * from commodities where ${key} = #{value}")
+    Commodities getCommodityByColumn(String key, String value);
+
+    @Update("update commodities set name = #{name}")
+    int updateMember(Commodities commodity);
+
+    @Delete("delete from commodities where id = #{id}")
+    int deleteCommodityById(int id);
+}
