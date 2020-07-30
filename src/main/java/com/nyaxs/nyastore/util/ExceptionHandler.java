946e2f6b157e711bdfab41298e08c663899790c8
nyaxs <nyaxs@outlook.com>
2020-07-30 23:02:08 +0800

    add ExceptionHandler

---
+++ src/main/java/com/nyaxs/nyastore/util/ExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.nyaxs.nyastore.util;
+
+/**
+ * @ClassName ExceptionHandler
+ * @Description TODO:
+ * @Author nyaxs
+ * @Date 2020-07-30 23:00
+ * @Version 1.0
+ **/
+public class ExceptionHandler {
+}
