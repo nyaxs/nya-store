3bb6784ade1e6b0c927e4372b3524909ccf5bdcd
nyaxs <nyaxs@outlook.com>
2020-07-30 22:59:40 +0800

    add ExceptionInfo entity

---
+++ src/main/java/com/nyaxs/nyastore/entity/exception/ExceptionInfo.java
@@ -0,0 +1,11 @@
+package com.nyaxs.nyastore.entity.exception;
+
+/**
+ * @ClassName ExceptionInfo
+ * @Description TODO:
+ * @Author nyaxs
+ * @Date 2020-07-30 22:58
+ * @Version 1.0
+ **/
+public class ExceptionInfo {
+}
