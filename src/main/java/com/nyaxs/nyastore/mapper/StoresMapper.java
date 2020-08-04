295bf7b4f1e60a6166e236a3b984167d4c7246e4
nyaxs <nyaxs@outlook.com>
2020-08-04 18:45:48 +0800

    --

--- src/main/java/com/nyaxs/nyastore/mapper/StoresMapper.java
+++ src/main/java/com/nyaxs/nyastore/mapper/StoresMapper.java
@@ -20,11 +20,28 @@ import java.util.List;
 @Mapper
 public interface StoresMapper {
 
+    /**
+     * todo: 根据指定的 id, 查找商店记录
+     * @param id 商店 id 参数
+     * @return 返回 一条 stores 记录对应的 Stores 对象
+     */
     @Select("select * from stores where id = #{id}")
     Stores getStoreById(int id);
 
+
+    /**
+     * todo: 根据指定的商店名，进行精确匹配对应的商店
+     * @param name 商店名参数
+     * @return 返回stores表一条记录对应的商店对象
+     */
     @Select("select * from stores where name = #{name}")
     Stores getStoreByName(String name);
+
+    /**
+     * todo: 根据指定的商店名，进行模糊查询
+     * @param name 传入的商店名参数
+     * @return 返回一组匹配的商店列表
+     */
 
     @Select("select * from stores where name like  concat('%',#{name},'%')  ")
     List<Stores> getStoresByName(String name);
@@ -39,6 +56,12 @@ public interface StoresMapper {
     int insertStore(Stores store);
 
 
+    /**
+     * todo: 根据 id 删除 stores 表中一条记录
+     * @alert: 危险操作，请验证权限
+     * @param id 商店的id
+     * @return int 影响行数
+     */
     @Delete("delete from stores where id = #{id}")
     int deleteStoreById(int id);
 
