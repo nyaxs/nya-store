f4fd8e3a63be5b30eac239722bdd223d471df6ce
nyaxs <nyaxs@outlook.com>
2020-08-10 22:41:12 +0800

    -

---
+++ src/main/java/com/nyaxs/nyastore/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.nyaxs.nyastore.config;
+
+/**
+ * @ClassName RedisConfig
+ * @Description TODO:
+ * @Author nyaxs
+ * @Date 2020-08-10 22:40
+ * @Version 1.0
+ **/
+
+public class RedisConfig {
+
+}
